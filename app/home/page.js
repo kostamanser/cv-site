62dba28f7d3afc04a029a0c01665512a34ca1b86
kostamanser <kostamanser@gmail.com>
2024-09-11 17:30:25 +0100

    Changed favicon, added collapsing bullets and fixed bugs

--- app/home/page.js
+++ app/home/page.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
+import cardData from '../components/Data';
 import NameCard from '../components/NameCard';
 import styles from './HomePage.module.css';
 import Image from 'next/image';
@@ -20,50 +21,24 @@ const variants = {
 export default function HomePage() {
   const addressee = 'there'; // CHANGE NAME OF ADDRESSEE
 
-  const exampleCards = [
-    {
-      title: 'Hi!',
-      summary: ['If you\'ve got 5 minutes to spare, below is my version of a CV and cover letter. It briefly covers my skils and mindsets and touches on me as a person.',
-        'In a nutshell, I am looking for a role where I can use my top-notch problem-solving, project management and data analysis skills.',
-        <span key="bold-text"> If you need someone like that, <strong>read on!</strong></span>
-      ],
-      bullets: [],
-      visible: false
-    },
-    {
-        title: 'Education',
-        summary: 'A brief overview of my academic background.',
-        bullets: ['PhD in Biology - University of Bristol', 'MSci in Biology (First Class)'],
-        visible: true
-    },
-    {
-      title: 'Soft Skills',
-      summary: 'Summary',
-      bullets: ['Teamwork', 'Skill 2', 'Skill 3'],
-      visible: true
-    },
-    {
-      title: 'Technical Skills',
-      summary: 'Key competencies and technical skills.',
-      bullets: ['Data Analysis (Python, R)', 'Project management', 'Research'],
-      visible: true
-    },
-  ];
-
-
-
   const [imageMoved, setImageMoved] = useState(false); // To track when the image is in place
   const [cardsVisible, setCardsVisible] = useState(false); // To trigger card fade-in
 
+  const [isVisible, setIsVisible] = useState(false); // Track visibility for "More Information" button
+  const [isRotated, setIsRotated] = useState(false); // Track rotation of bullets
+
+    // Toggle more information and bullet rotation
+    const handleToggle = () => {
+      setIsVisible(!isVisible);
+      setIsRotated(!isRotated);
+    };
 
   useEffect(() => {
     // After the image moves, trigger card fade-ins
     const timer = setTimeout(() => {
       setImageMoved(true);
       // Show cards after a short delay
-      setTimeout(() => {
-        
-        console.log('Setting cardsVisible to true');
+      setTimeout(() => {        
         setCardsVisible(true);
       }, 2200); // Adjust this timing as needed
     }, 1000); // Adjust time to match the desired duration for the profile image movement
@@ -89,7 +64,7 @@ export default function HomePage() {
         <NameCard />
         {/* New Wrapper for the Cards */}
         <div className={styles.cardsWrapper}>
-        {exampleCards.map((card, index) => (
+        {cardData.map((card, index) => (
           <motion.div
             key={index}
             initial="hidden"
@@ -102,6 +77,9 @@ export default function HomePage() {
               summary={card.summary}
               bullets={card.bullets}
               visible={card.visible} // Pass the visible property here
+              isRotated={isRotated} // Pass the rotation state
+              isVisible={isVisible} // Pass the visibility state for more info
+              handleToggle={handleToggle} // Pass the toggle function
             />
           </motion.div>
           ))}
